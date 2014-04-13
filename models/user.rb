0822ec278df7d1c80f3551fe8d5ad8cc595d95d0
luckypool <luckypool314@gmail.com>
2014-04-13 23:46:41 +0900

    Add validations to user model

--- models/user.rb
+++ models/user.rb
@@ -1,5 +1,7 @@
 require "date"
 class User < ActiveRecord::Base
+  validates :name, presence: true
+  validates :birthday, format: { with: /\A\d{4}-\d{2}-\d{2}\z/}
   def age
     birthday = Date.parse(self.birthday.to_s)
     Date.today.year - birthday.year
